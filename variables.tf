652796a57299caaf4086b9f2bc557204dac40286
kusumsiri <kusumsiri@gmail.com>
2023-05-27 16:53:00 +0530

    Adding viriables.tf

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "runtime" {
+    description = "lambda function runtime env"
+    type = string
+    default = "nodejs18.x"
+}
+
+variable "function_name" {
+    description = "Provide name to the lambda function"
+    type = string
+    default = "news-translater"
+}
+
+variable "bucket_name" {
+    description = "Provide the details of S3 Bucket where the lambda fuction code is stored"
+    type = string
+    default = "kusumsiri-test-bucket"
+}
+
+variable "log_retention_in_days" {
+    description = "Provide the number of days for log to be available in cloudwatch for this lambda function"
+    type = number
+    default = 1
+}
+
+variable "news_api_key" {
+    description = "The news api key"
+    type = string
+    sensitive = true
+}
+
